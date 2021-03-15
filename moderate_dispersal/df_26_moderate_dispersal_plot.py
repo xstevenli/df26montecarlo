c7dac13801b518e71f35fdb3c7bcd45a308412ea
xstevenli <80720868+xstevenli@users.noreply.github.com>
2021-03-15 15:08:24 -0700

    Add files via upload

--- moderate_dispersal/df_26_moderate_dispersal_plot.py
+++ moderate_dispersal/df_26_moderate_dispersal_plot.py
@@ -9,8 +9,11 @@ x, y = zip(*lists)
 
 fig = plt.figure()
 plt.plot(x, y)
-fig.suptitle('20 DF-26s Striking 24 Open Parking Spots\CEP = 50', fontsize=14)
+fig.suptitle('20 DF-26s Striking 24 Open Parking Spots\nCEP = 50 m', fontsize=14)
+plt.xlim([0, 20])
+plt.ylim([0, 24])
 plt.xlabel('Leaked DF-26 IRBMs', fontsize=14)
 plt.ylabel('Aircraft Destroyed', fontsize=14)
 plt.grid()
 plt.show()
+

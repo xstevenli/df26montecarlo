fd94f10a89485fe968b2702f2e87db7ee5c50edc
xstevenli <80720868+xstevenli@users.noreply.github.com>
2021-03-15 14:27:28 -0700

    Add files via upload

---
+++ baseline/df_26_baseline_plot_shelter.py
@@ -0,0 +1,16 @@
+import pickle
+import matplotlib.pylab as plt
+
+df_26_baseline = pickle.load(open("df_26_baseline_shelter.p", "rb"))
+
+lists = sorted(df_26_baseline.items())
+
+x, y = zip(*lists)
+
+fig = plt.figure()
+plt.plot(x, y)
+fig.suptitle('100 DF-26s Striking 120 RDS Parking Spots\CEP = 50', fontsize=14)
+plt.xlabel('Leaked DF-26 IRBMs', fontsize=14)
+plt.ylabel('Aircraft Destroyed', fontsize=14)
+plt.grid()
+plt.show()
---
+++ moderate_dispersal/df_26_moderate_dispersal_plot_shelter.py
@@ -0,0 +1,16 @@
+import pickle
+import matplotlib.pylab as plt
+
+df_26_baseline = pickle.load(open("df_26_moderate_dispersal_shelter.p", "rb"))
+
+lists = sorted(df_26_baseline.items())
+
+x, y = zip(*lists)
+
+fig = plt.figure()
+plt.plot(x, y)
+fig.suptitle('20 DF-26s Striking 24 RDS Parking Spots\CEP = 50', fontsize=14)
+plt.xlabel('Leaked DF-26 IRBMs', fontsize=14)
+plt.ylabel('Aircraft Destroyed', fontsize=14)
+plt.grid()
+plt.show()

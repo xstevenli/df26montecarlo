7d4080b43cef70d8814a8d2875533b5f8c58d848
xstevenli <80720868+xstevenli@users.noreply.github.com>
2021-03-15 15:06:49 -0700

    Add files via upload

--- baseline/df_26_baseline_plot.py
+++ baseline/df_26_baseline_plot.py
@@ -9,7 +9,7 @@ x, y = zip(*lists)
 
 fig = plt.figure()
 plt.plot(x, y)
-fig.suptitle('100 DF-26s Striking 120 Open Parking Spots\CEP = 50', fontsize=14)
+fig.suptitle('100 DF-26s Striking 120 Open Parking Spots\nCEP = 50 m', fontsize=14)
 plt.xlabel('Leaked DF-26 IRBMs', fontsize=14)
 plt.ylabel('Aircraft Destroyed', fontsize=14)
 plt.grid()

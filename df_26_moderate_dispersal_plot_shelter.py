58788a5ffec2ba10f34ab220b96db7891e00aa84
xstevenli <80720868+xstevenli@users.noreply.github.com>
2021-03-15 15:45:50 -0700

    Add files via upload

---
+++ df_26_moderate_dispersal_plot_shelter.py
@@ -0,0 +1,17 @@
+import pickle
+import matplotlib.pylab as plt
+
+df_26_baseline = pickle.load(open("df_26_moderate_dispersal_shelter.p", "rb"))
+
+lists = sorted(df_26_baseline.items())
+
+x, y = zip(*lists)
+
+fig = plt.figure()
+plt.plot(x, y)
+fig.suptitle('20 DF-26s Striking 24 RDS Parking Spots\nCEP = 50 m', fontsize=14)
+plt.xticks(np.arrange(0,20,4))
+plt.xlabel('Leaked DF-26 IRBMs', fontsize=14)
+plt.ylabel('Aircraft Destroyed', fontsize=14)
+plt.grid()
+plt.show()
